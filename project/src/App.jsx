3ac0ed28c0fa1063eafe29a0d1cc7e562ed4f1b2
MichaelHanz <drunderpant@gmail.com>
2025-10-28 12:49:52 +0800

    downloading the neccessary packages

---
+++ project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+    <h1 className="text-3xl font-bold underline text-amber-800">
+      Hello, Tailwind CSS!
+    </h1>
+  )
+}
+
+export default App
